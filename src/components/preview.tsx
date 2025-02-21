82e7e38cb496bd5641149532133fc9addfb33c37
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-02-21 09:17:45 -0600

    horizontal resizing refactor

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -13,13 +13,16 @@ const html = `
       <body>
         <div id="root"></div>
         <script>
+          const handleError = (err) => {
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+            console.error(err);
+          } 
           window.addEventListener('message', (event) => {
             try {
               eval(event.data);
             } catch (err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-              console.error(err);
+              handleError(err);
             }
           }, false);
         </script>
@@ -33,7 +36,9 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   useEffect(() => {
     iframe.current.srcdoc = html;
     iframe.current.onload = () => {
-      iframe.current.contentWindow.postMessage(code, "*");
+      setTimeout(() => {
+        iframe.current.contentWindow.postMessage(code, "*");
+      }, 50);
     };
   }, [code]);
 
