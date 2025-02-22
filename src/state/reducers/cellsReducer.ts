3c19f54590e94d71f2b2c327806bd5eb3cc034be
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-02-22 16:40:36 -0600

    update, delete and move cell reducer

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,6 +1,7 @@
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
 import { Cell } from "../cell";
+import produce from "immer";
 
 interface CellsState {
   loading: boolean;
@@ -18,22 +19,33 @@ const initialState: CellsState = {
   data: {},
 };
 
-const reducer = (
-  state: CellsState = initialState,
-  action: Action
-): CellsState => {
+const reducer = produce((state: CellsState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
-      return state;
+      const { id, content } = action.payload;
+      state.data[id].content = content;
+      return;
     case ActionType.DELETE_CELL:
-      return state;
+      delete state.data[action.payload];
+      state.order = state.order.filter((id) => id !== action.payload);
+      return;
     case ActionType.MOVE_CELL:
-      return state;
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === "up" ? index - 1 : index + 1;
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return;
+      }
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+      return;
     case ActionType.INSERT_CELL_BEFORE:
       return state;
     default:
       return state;
   }
-};
+});
 
 export default reducer;
